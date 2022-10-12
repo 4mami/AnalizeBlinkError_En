14a293c9a2e15aca243cbc89e5c9de0540e815d7
4mami <librarywar2019@icloud.com>
2022-10-12 22:01:19 +0900

    [add] RoughenCategory()のテストケースを追加

--- tests/test_PreProcessCirrusDump.py
+++ tests/test_PreProcessCirrusDump.py
@@ -2,20 +2,15 @@ import unittest
 import PreProcessCirrusDump
 
 class MyTest(unittest.TestCase):
-    old_categories = []
-
-    def setUp(self) -> None:
-        self.old_categories = list(map(lambda c: c.lower(), [
+    def test_RoughenCategory_1(self):
+        old_categories = list(map(lambda c: c.lower(), [
             'Scooby-Doo', 
             'Fictional characters introduced in 1969', 
             'Media franchises', 
             'Television programs adapted into films', 
             'Television programs adapted into comics'
         ]))
-        return super().setUp()
-
-    def test_RoughenCategory(self):
-        self.assertEqual(sorted(PreProcessCirrusDump.RoughenCategory(self.old_categories)), [
+        self.assertEqual(sorted(PreProcessCirrusDump.RoughenCategory(old_categories)), [
             '1969', 
             'adapted', 
             'characters', 
@@ -29,6 +24,27 @@ class MyTest(unittest.TestCase):
             'programs', 
             'scooby-doo', 
             'television'
+        ])
+
+    def test_RoughenCategory_2(self):
+        old_categories = list(map(lambda c: c.lower(), [
+            'Nose',
+            'Human head and neck',
+            'Respiratory system',
+            'Olfactory system',
+            'Facial features'
+        ]))
+        self.assertEqual(sorted(PreProcessCirrusDump.RoughenCategory(old_categories)), [
+            'and',
+            'facial',
+            'features',
+            'head',
+            'human',
+            'neck',
+            'nose',
+            'olfactory',
+            'respiratory',
+            'system'
         ])
 
 if __name__ == '__main__':
