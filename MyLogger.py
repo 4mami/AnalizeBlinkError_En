fb4f543ae924b2c094fc54fe252edadc265ffe12
4mami <librarywar2019@icloud.com>
2022-10-12 22:09:38 +0900

    [clean] 半角スペース挿入

--- MyLogger.py
+++ MyLogger.py
@@ -3,7 +3,7 @@ from logging import config, getLogger
 
 class MyLogger:
     @staticmethod
-    def initialize(logger_name:str, config_path: str):
+    def initialize(logger_name: str, config_path: str):
         with open(config_path, 'r') as f:
             log_conf = json.load(f)
         config.dictConfig(log_conf)
