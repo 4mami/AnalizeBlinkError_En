7cca394e649427cab3afe5a80783faf90f638a8e
4mami <librarywar2019@icloud.com>
2022-12-05 17:23:01 +0900

    [delete] Delete tmp code

--- ReadUnseenMentions.py
+++
@@ -1,9 +0,0 @@
-import json
-
-file_path = '../Dataset/unseen_mentions/test.json'
-out = []
-with open(file_path, "r") as file:
-    for line in file:
-        data: dict = json.loads(line)
-        out.append(data)
-print("finished")
